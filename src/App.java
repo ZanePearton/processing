1fb9dbb317d7098ced679d9da9cf0e5fe0507b98
Zane <z.pearton@accenture.com>
2023-06-27 17:51:57 +1000

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+import processing.core.PApplet;
+
+public class App extends PApplet{
+
+    PApplet processing;
+    Random random = new Random();
+    int numberOfworkers = 30;
+    public int screenWidth = 1200;
+    public int screenHeight = 800;
+    public int buildingWidth = 100; // x axis
+    public int buildingDepth = 100; // y axis
+    public int buildingHeight = 200; // z axis
+
+    public void settings(){
+        size(screenWidth, screenHeight, P3D);
+        mainLoop();
+    }
+
+    public void mainLoop(){
+    }
+
+    public void setup(){
+        background(255);
+        colorMode(HSB);
+    }
+
+    public void draw(){
+        background(255);
+    }
+
+    public static void main(String[] args) {
+        String[] processingArgs = { "test" };
+        App appMain = new App();
+        PApplet.runSketch(processingArgs, appMain);
+    }
+
+}
