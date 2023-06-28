b09f34f21748ed444302f758ccf913e75fa47a22
Zane <z.pearton@accenture.com>
2023-06-28 14:36:10 +1000

    revised

--- src/behaviour/randomWalkerClass.java
+++ src/behaviour/randomWalkerClass.java
@@ -57,7 +57,6 @@ public class randomWalkerClass {
         System.out.println(loc.x + "," + loc.y + "," + loc.z);
         String outputPath = "output.csv";
         try {
-
             FileWriter fileWriter = new FileWriter(outputPath,true);
             try (BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
                 bufferedWriter.write(loc.x + "," + loc.y + "," + loc.z);
@@ -65,7 +64,6 @@ public class randomWalkerClass {
                 // bufferedWriter.close();   
                 // System.out.println("CSV file created successfully."); 
             }
-
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
